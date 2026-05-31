c7bc22a7dbe62a511b805aaa19ab66838dc315e6
rahulkumar1165 <rahulkr1165@gmail.com>
2026-05-31 10:30:24 +0530

    besent-code

--- main.tf
+++ main.tf
@@ -1,4 +1,20 @@
-# Configure the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.11.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
+}
+
+resource "aws_instance" "myserver" {
+  ami           = "ami-068c0051b15cdb816"
+  instance_type = "t3.micro"
+
+  tags = {
+  Name = "Rahul-Besent" }
 }
